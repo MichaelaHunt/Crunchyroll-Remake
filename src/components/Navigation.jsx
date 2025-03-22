3b3a046138f75d19d3cd8a520701109bb182b166
Michaela Hunt <micapplehunt@gmail.com>
2025-03-22 16:09:05 -0400

    navigation completed

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,17 @@
 function Navigation() {
-    return(
+    return (
         <>
             <div className="navContainer">
-                
+                <div className="navCategories">
+                    <h3>Not Crunchyroll</h3>
+                    <h4>This Season</h4>
+                    <h4>Browse By</h4>
+                    <h4>Watchlist</h4>
+                    <h4>Subscribed</h4>
+                </div>
+                <div className="User">
+                    <i></i>
+                </div>
             </div>
         </>
     );
